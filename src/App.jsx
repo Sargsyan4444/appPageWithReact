8810f807c9a6f0b089252b94300926d5ea54e799
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-04-15 14:35:13 +0300

    i added collection part

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Collection from './Collection';
 import GreatCoffe from './GreatCoffe';
 import Header from './Header';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <GreatCoffe/>
+      <Collection/>
     </div>
   );
 }
---
+++ src/Collection.jsx
@@ -0,0 +1,41 @@
+function Collection() {
+    return (
+        <section className="bg-[#FEFCF7] w-full font-winky">
+        <div className="flex flex-col items-center justify-center">
+            <img src="/images/collection/collMain.png" alt="" className="w-full max-w-[1111px] h-auto object-contain mt-[136px] block" />
+            <div className="flex items-center justify-center gap-[30px] flex-wrap mt-[-50px] pb-[200px]">
+                <div className="h-[400px] w-[234px] flex items-center ustify-center flex-col">
+                    <img src="/images/collection/kopi1 copy (1).png" alt="" className="w-[234px] h-[177px] object-contain mt-[75px]" />
+                    <div className="flex items-center justify-center flex-col">
+                        <h4 className="text-[#333D4B] font-black text-2xl">Planalto</h4>
+                        <p className="text-[#333D4B] pt-[24px] text-base font-normal items-center">Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.</p>
+                    </div>
+                </div>
+                <div className="h-[400px] w-[234px] flex items-center ustify-center flex-col">
+                    <img src="/images/collection/kopi2.png" alt="" className="w-[234px] h-[177px] object-contain mt-[75px]" />
+                    <div className="flex items-center justify-center flex-col">
+                        <h4 className="text-[#333D4B] font-black text-2xl">Planalto</h4>
+                        <p className="text-[#333D4B] pt-[24px] text-base font-normal items-center">Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.</p>
+                    </div>
+                </div>
+                <div className="h-[400px] w-[234px] flex items-center ustify-center flex-col">
+                    <img src="/images/collection/kopi3 (1).png" alt="" className="w-[234px] h-[177px] object-contain mt-[75px]" />
+                    <div className="flex items-center justify-center flex-col">
+                        <h4 className="text-[#333D4B] font-black text-2xl">Piccollo</h4>
+                        <p className="text-[#333D4B] pt-[24px] text-base font-normal items-center">Mild and smooth blend featuring notes of toasted almond and dried cherry.</p>
+                    </div>
+                </div>
+                <div className="h-[400px] w-[234px] flex items-center ustify-center flex-col">
+                    <img src="/images/collection/kopi4 (1).png" alt="" className="w-[234px] h-[177px] object-contain mt-[75px]" />
+                    <div className="flex items-center justify-center flex-col">
+                        <h4 className="text-[#333D4B] font-black text-2xl">Danche</h4>
+                        <p className="text-[#333D4B] pt-[24px] text-base font-normal items-center">Ethiopian hand-harvested blend densely packed with vibrant fruit notes.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Collection
